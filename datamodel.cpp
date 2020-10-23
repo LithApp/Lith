556629df70d02888698b590de6e6ede2f27b4472
Martin Bříza <m@rtinbriza.cz>
2020-10-23 20:11:08 +0200

    Basic color handling

--- datamodel.cpp
+++ datamodel.cpp
@@ -27,6 +27,26 @@ QDataStream &W::operator>>(QDataStream &s, W::LongInteger &r) {
     return s;
 }
 
+static const QMap<int, QString> weechatColors {
+    { 0, "" },
+    { 1, "black" },
+    { 2, "dark gray" },
+    { 3, "#8b0000" },
+    { 4, "#ffcccb" },
+    { 5, "dark green" },
+    { 6, "light green" },
+    { 7, "brown" },
+    { 8, "yellow" },
+    { 9, "#00008b" },
+    { 10, "#add8e6" },
+    { 11, "#8b008b" },
+    { 12, "#ff80ff" },
+    { 13, "#008b8b" },
+    { 14, "#e0ffff" },
+    { 15, "gray" },
+    { 16, "white" }
+};
+
 QDataStream &W::operator>>(QDataStream &s, W::String &r) {
     uint32_t len;
     r.d.clear();
@@ -36,8 +56,7 @@ QDataStream &W::operator>>(QDataStream &s, W::String &r) {
     else if (len == 0)
         r.d = "";
     else if (len > 0) {
-        QByteArray buf(len + 1, 0);
-        s.readRawData(buf.data(), len);
+        /*
         int i;
         while ((i = buf.indexOf((char) 0x1a)) >= 0) {
             buf.replace(i, 2, "");
@@ -55,6 +74,290 @@ QDataStream &W::operator>>(QDataStream &s, W::String &r) {
                 buf.replace(i, 3, "");
         }
         r.d = buf;
+        */
+        QByteArray buf(len + 1, 0);
+        s.readRawData(buf.data(), len);
+        r.d.clear();
+        bool foreground = false;
+        bool background = false;
+        bool bold = false;
+        bool reverse = false;
+        bool italic = false;
+        bool underline = false;
+        bool keep = false;
+        auto endTags = [&r, &foreground, &background, &bold, &reverse, &italic, &underline, &keep]() {
+            if (foreground) {
+                foreground = false;
+                r.d += "</font>";
+            }
+            if (background) {
+                background = false;
+                r.d += "</span>";
+            }
+            if (bold) {
+                bold = false;
+                r.d += "</b>";
+            }
+            if (reverse) {
+                reverse = false;
+                // TODO
+            }
+            if (italic) {
+                italic = false;
+                r.d += "</i>";
+            }
+            if (underline) {
+                underline = false;
+                r.d += "</u>";
+            }
+            if (keep) {
+                // TODO
+            }
+        };
+        auto loadAttr = [&r, &bold, &reverse, &italic, &underline, &keep](QByteArray::iterator &it) {
+            while (true) {
+                switch(*it) {
+                case '*':
+                    if (bold)
+                        break;
+                    bold = true;
+                    r.d += "<b>";
+                    break;
+                case '!':
+                    if (reverse)
+                        break;
+                    reverse = true;
+                    // TODO
+                    break;
+                case '/':
+                    if (italic)
+                        break;
+                    italic = true;
+                    r.d += "<i>";
+                    break;
+                case '_':
+                    if (underline)
+                        break;
+                    underline = true;
+                    r.d += "<u>";
+                    break;
+                case '|':
+                    break;
+                case '@':
+                    break;
+                case 0x1A:
+                    break;
+                case 0x19:
+                    break;
+                default:
+                    --it;
+                    return;
+                }
+                ++it;
+            }
+        };
+
+        auto clearAttr = [&r, &bold, &reverse, &italic, &underline, &keep](QByteArray::iterator &it) {
+            while (true) {
+                switch(*it) {
+                case '*':
+                    if (bold) {
+                        bold = false;
+                        r.d += "</b>";
+                    }
+                    break;
+                case '!':
+                    if (reverse) {
+                        reverse = false;
+                        // TODO
+                    }
+                    break;
+                case '/':
+                    if (italic) {
+                        italic = false;
+                        r.d += "</i>";
+                    }
+                    break;
+                case '_':
+                    if (underline) {
+                        underline = false;
+                        r.d += "</u>";
+                    }
+                    break;
+                case '|':
+                    break;
+                case 0x1B:
+                    break;
+                default:
+                    --it;
+                    return;
+                }
+                ++it;
+            }
+        };
+        auto loadStd = [&r, &foreground](QByteArray::iterator &it) {
+            while (*it == '@' || *it == '*' || *it == '!' || *it == '/' || *it == '_' || *it == '|')
+                ++it;
+            int code = 0;
+            if (*it == 0x19 || *it == 'F')
+                it++;
+            for (int i = 0; i < 2; i++) {
+                code *= 10;
+                code += (*it) - '0';
+                ++it;
+            }
+            --it;
+            if (foreground)
+                r.d += "</font>";
+            if (weechatColors.contains(code)) {
+                r.d += "<font color=\""+weechatColors[code]+"\">";
+                foreground = true;
+            }
+        };
+        auto loadExt = [&r, &foreground](QByteArray::iterator &it) {
+            while (*it == '@' || *it == '*' || *it == '!' || *it == '/' || *it == '_' || *it == '|')
+                ++it;
+            int code = 0;
+            for (int i = 0; i < 5; i++) {
+                code *= 10;
+                code += (*it) - '0';
+                ++it;
+            }
+            --it;
+            if (foreground)
+                r.d += "</font>";
+            r.d += "<font color=\"green\">";
+            foreground = true;
+        };
+        auto loadBgStd = [&r, &background](QByteArray::iterator &it) {
+            while (*it == '@' || *it == '*' || *it == '!' || *it == '/' || *it == '_' || *it == '|')
+                ++it;
+            int code = 0;
+            for (int i = 0; i < 2; i++) {
+                code *= 10;
+                code += (*it) - '0';
+                ++it;
+            }
+            --it;
+            if (background)
+                r.d += "</span";
+            r.d += "<span style=\"background-color: blue\">";
+            background = true;
+        };
+        auto loadBgExt = [&r, &background](QByteArray::iterator &it) {
+            while (*it == '@' || *it == '*' || *it == '!' || *it == '/' || *it == '_' || *it == '|')
+                ++it;
+            int code = 0;
+            for (int i = 0; i < 5; i++) {
+                code *= 10;
+                code += (*it) - '0';
+                ++it;
+            }
+            --it;
+            if (background)
+                r.d += "</span";
+            r.d += "<span style=\"background-color: red\">";
+            background = true;
+        };
+        auto getChar = [](QByteArray::iterator &it) -> QString {
+            if ((unsigned char) *it < 0x80) {
+                return QString(*it);
+            }
+            else {
+                QByteArray buf;
+                if ((*it & 0b11111000) == 0b11110000) {
+                    buf += *it++;
+                    buf += *it++;
+                    buf += *it++;
+                    buf += *it;
+                }
+                else if ((*it & 0b11110000) == 0b11100000) {
+                    buf += *it++;
+                    buf += *it++;
+                    buf += *it;
+                }
+                else if ((*it & 0b11100000) == 0b11000000) {
+                    buf += *it++;
+                    buf += *it;
+                }
+                else {
+                    return QString(*it);
+                }
+                return QString(buf);
+            }
+        };
+        for (auto it = buf.begin(); it != buf.end(); ++it) {
+            if (*it == 0x19) {
+                ++it;
+                if (*it == 'F') {
+                    ++it;
+                    if (*it == '@') {
+                        ++it;
+                        loadAttr(it);
+                        loadExt(it);
+                    }
+                    else {
+                        loadAttr(it);
+                        loadStd(it);
+                    }
+                }
+                else if (*it == 'B') {
+                    ++it;
+                    if (*it == '@')
+                        loadBgExt(it);
+                    else
+                        loadBgStd(it);
+                }
+                else if (*it == '*') {
+                    ++it;
+                    if (*it == '@') {
+                        ++it;
+                        loadAttr(it);
+                        loadExt(it);
+                    }
+                    else {
+                        loadAttr(it);
+                        loadExt(it);
+                    }
+                    if (*it == ',' || *it == '~')
+                        ++it;
+                    if (*it == '@') {
+                        ++it;
+                        loadAttr(it);
+                        loadExt(it);
+                    }
+                    else {
+                        loadAttr(it);
+                        loadExt(it);
+                    }
+                }
+                else if (*it == '@') {
+                    ++it;
+                    loadExt(it);
+                }
+                else if (*it == 0x1C) {
+                    endTags();
+                }
+                else {
+                    loadStd(it);
+                }
+            }
+            else if (*it == 0x1C) {
+                ++it;
+                endTags();
+            }
+            else if (*it == 0x1A) {
+                ++it;
+                loadAttr(it);
+            }
+            else if (*it == 0x1B) {
+                clearAttr(it);
+            }
+            else if (*it) {
+                r.d += getChar(it);
+            }
+        }
+        endTags();
     }
     return s;
 }
