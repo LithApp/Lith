5d8c9128b2bfb76ed6d3252417a37b0815a1f933
Martin Bříza <m@rtinbriza.cz>
2020-10-25 19:39:10 +0100

    Add an iOS CI build

--- dist/get-tag-name.sh
+++ dist/get-tag-name.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROBABLY_TAG=${GITHUB_REF/refs\/tags\//}
 
-TAG=$(grep "^[0-9][0-9]*[.][0-9][0-9]*-[0-9][0-9]*$" <<< "$PROBABLY_TAG")
+TAG=$(grep "^[0-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]*$" <<< "$PROBABLY_TAG")
 
 # Default tag
 #if [ -z "$TAG" ]; then
