e1f83f4672d01ae917ee79522f60ccd7087a51c6
Martin Bříza <m@rtinbriza.cz>
2020-11-09 20:36:08 +0100

    Fix iOS CI build

--- dist/ios/build.sh
+++ dist/ios/build.sh
@@ -10,5 +10,5 @@ $Qt5_Dir/bin/qmake .. VERSION=$TAG_NAME QMAKE_DEVELOPMENT_TEAM=Z52EFCPL6D
 xcodebuild -project Lith.xcodeproj -sdk iphoneos -configuration Release clean
 xcodebuild -project Lith.xcodeproj -scheme Lith -sdk iphoneos -configuration Release archive -archivePath Lith.xcarchive
 xcodebuild -exportArchive -archivePath Lith.xcarchive -exportOptionsPlist ../dist/ios/exportOptions.plist -exportPath .
-xcrun altool --validate-app --file Lith.ipa --username "$APPLEID_NAME" --password "$APPLEID_PASSWORD"
-xcrun altool --upload-app --file Lith.ipa --username "$APPLEID_NAME" --password "$APPLEID_PASSWORD"
+
+cd ..
