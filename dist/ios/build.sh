36cc039125a9e77b383d6bcca95bdf174e93cc88
Martin Bříza <m@rtinbriza.cz>
2021-10-04 22:04:06 +0200

    Fix CI build for Qt 6.2 - at least on desktop

--- dist/ios/build.sh
+++ dist/ios/build.sh
@@ -5,7 +5,7 @@ set -x
 mkdir build_ios
 cd build_ios
 
-$Qt5_Dir/bin/qmake .. VERSION=$TAG_NAME QMAKE_DEVELOPMENT_TEAM=Z52EFCPL6D IMGUR_API_KEY=$IMGUR_API_KEY
+$Qt6_DIR/bin/qmake .. VERSION=$TAG_NAME QMAKE_DEVELOPMENT_TEAM=Z52EFCPL6D IMGUR_API_KEY=$IMGUR_API_KEY
 
 xcodebuild -project Lith.xcodeproj -sdk iphoneos -configuration Release clean
 xcodebuild -project Lith.xcodeproj -scheme Lith -sdk iphoneos -configuration Release archive -archivePath Lith.xcarchive
--- dist/macos/build.sh
+++ dist/macos/build.sh
@@ -2,5 +2,5 @@
 
 mkdir build_macos
 cd build_macos
-$Qt5_Dir/bin/qmake .. IMGUR_API_KEY=$IMGUR_API_KEY VERSION=$TAG_NAME
+$Qt6_DIR/bin/qmake .. IMGUR_API_KEY=$IMGUR_API_KEY VERSION=$TAG_NAME
 make -j2
