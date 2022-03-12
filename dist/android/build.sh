5e953c2433bab53521d5882a3849fe505938c84d
Martin Bříza <m@rtinbriza.cz>
2022-03-12 09:33:58 +0100

    Fix Android CI build

--- dist/android/build.sh
+++ dist/android/build.sh
@@ -7,10 +7,10 @@ pushd build_android
 mkdir package
 
 #mkdir /home/user/build/dist/
-${Qt6_DIR}/bin/qmake -r .. CONFIG+=debug ANDROID_EXTRA_LIBS+=../android_openssl/latest/arm64/libcrypto_1_1.so ANDROID_EXTRA_LIBS+=../android_openssl/latest/arm64/libssl_1_1.so QMAKE_CFLAGS+=-funwind-tables QMAKE_CXXFLAGS+=-funwind-tables IMGUR_API_KEY=$IMGUR_API_KEY
+${Qt6_DIR}/bin/qmake -r .. CONFIG+=debug ANDROID_EXTRA_LIBS+=../android_openssl/latest/arm/libcrypto_1_1.so ANDROID_EXTRA_LIBS+=../android_openssl/latest/arm/libssl_1_1.so QMAKE_CFLAGS+=-funwind-tables QMAKE_CXXFLAGS+=-funwind-tables IMGUR_API_KEY=$IMGUR_API_KEY
 make -j5
 make apk_install_target
 #make install INSTALL_ROOT=/home/user/build/dist/
 
-${Qt6_DIR}/bin/androiddeployqt --input android-Lith-deployment-settings.json --output android-build/ --android-platform 29 --deployment bundled --gradle --debug
+${Qt6_DIR}/../gcc_64/bin/androiddeployqt --input android-Lith-deployment-settings.json --output android-build/ --android-platform 30 --deployment bundled --gradle --debug
 
