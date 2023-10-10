c60563d106a02182feb7e2035036f27def2c93e7
Martin Bříza <m@rtinbriza.cz>
2023-10-10 19:57:22 +0200

    Hmmm

--- dist/macos/build.sh
+++ dist/macos/build.sh
@@ -3,5 +3,5 @@
 mkdir build_macos
 cd build_macos
 export CMAKE_PREFIX_PATH=$Qt6_DIR/lib/cmake
-cmake .. -DCMAKE_BUILD_TYPE=Release -DIMGUR_API_KEY=$IMGUR_API_KEY -DVERSION=$TAG_NAME
-make -j2
+cmake .. -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" -DCMAKE_BUILD_TYPE=Release -DIMGUR_API_KEY=$IMGUR_API_KEY -DVERSION=$TAG_NAME -DCMAKE_INSTALL_PREFIX=installed
+cmake --build . --parallel
