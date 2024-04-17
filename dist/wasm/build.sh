a286c38693a44732d1c46225e3f1a27ab3353835
Martin Bříza <m@rtinbriza.cz>
2024-04-17 18:25:13 +0200

    Update dependencies in CI and CMake
    Qt 6.7.2 (except WASM)
    Qt 6.6.3 (WASM)
    qcoro 0.10.0
    qtkeychain 0.14.3
    hopefully updated all github actions to versions that won't expire any time soon

--- dist/wasm/build.sh
+++ dist/wasm/build.sh
@@ -5,8 +5,8 @@ set -x
 mkdir build_wasm
 pushd build_wasm
 
-export QT_HOST_PATH="${Qt6_DIR}/../gcc_64"
-export CMAKE_PREFIX_PATH=$Qt6_DIR/lib/cmake
-cmake .. -DCMAKE_BUILD_TYPE=Release -DIMGUR_API_KEY=$IMGUR_API_KEY -DCMAKE_INSTALL_PREFIX=installed -DCMAKE_TOOLCHAIN_FILE=$Qt6_DIR/lib/cmake/Qt6/qt.toolchain.cmake
+export QT_HOST_PATH="${QT_ROOT_DIR}/../gcc_64"
+export CMAKE_PREFIX_PATH=$QT_ROOT_DIR/lib/cmake
+cmake .. -DCMAKE_BUILD_TYPE=Release -DIMGUR_API_KEY=$IMGUR_API_KEY -DCMAKE_INSTALL_PREFIX=installed -DCMAKE_TOOLCHAIN_FILE=$QT_ROOT_DIR/lib/cmake/Qt6/qt.toolchain.cmake
 cmake --build .
 cmake --install .
