e46d34544ed3ad6fbe95b9bb389689881c15ed58
Martin Bříza <m@rtinbriza.cz>
2024-06-29 12:43:44 +0200

    Implement a few rudimentary tests for the protocol implementation

--- modules/Lith/Core/test/ColorTest.cpp
+++ modules/Lith/Core/test/ColorTest.cpp
@@ -1,3 +1,19 @@
+// Lith
+// Copyright (C) 2024 Martin Bříza
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; If not, see <http://www.gnu.org/licenses/>.
+
 #include <QtTest/QtTest>
 
 #include "formattedstring.h"
@@ -14,7 +30,6 @@ private slots:
                                       "<span style='white-space: pre-wrap;'>"
                                       "</span>"
                                   );
-#if 1
         QTest::newRow("time_open") << QByteArray(
                                           "\x19\x30\x32"
                                           "12"
@@ -396,9 +411,6 @@ private slots:
                                        "<span style='background: #ff4444'><font color=\"#dddd00\">AHOJ</font></span>"
                                        "</span>"
                                    );
-
-
-#endif
     }
     void convertColors() {
         QFETCH(QByteArray, input);
