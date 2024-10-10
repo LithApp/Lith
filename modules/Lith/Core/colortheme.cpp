8c332c53993fe1d0535d90f06e47bc84244a398c
Martin Bříza <m@rtinbriza.cz>
2024-10-10 19:57:42 +0200

    Tweak the color palette, introduce other secondary colors

--- modules/Lith/Core/colortheme.cpp
+++ modules/Lith/Core/colortheme.cpp
@@ -25,19 +25,21 @@ QPalette ColorTheme::palette() const {
     // TODO very likely needs a bit of tweaking to differentiate button, window and base
     QColor windowText {m_weechatColors[0]};
     QColor base = (windowText == Qt::white) ? Qt::black : Qt::white;
-    QColor window = (base == Qt::white)                         ? QColor(0xc2, 0xbd, 0xb8)
-                    : (QColor(m_weechatColors[1]) == Qt::black) ? QColor(0x8, 0x7, 0x6)
+    QColor window = (base == Qt::white)                         ? QColor(0xbf, 0xbd, 0xbb)
+                    : (QColor(m_weechatColors[1]) == Qt::black) ? QColor(0x0A, 0x07, 0x0A)
                                                                 : m_weechatColors[1];
-    QColor button = mix(windowText, window, 0.15);
+    QColor button = mix(windowText, window, 0.20);
     QColor light = mix(windowText, button, 0.4);
     QColor dark = mix(window, windowText, 0.4);
     QColor mid = mix(light, dark);
     QColor text = windowText;
     QColor bright_text = text;
     QPalette palette {windowText, button, light, dark, mid, text, bright_text, base, window};
-    palette.setBrush(QPalette::Highlight, QColor(0xcc, 0x33, 0x33));
-    palette.setBrush(QPalette::HighlightedText, Qt::white);
-    palette.setBrush(QPalette::AlternateBase, mix(base, windowText, 0.9));
+    palette.setBrush(QPalette::BrightText, QColor(0x8d, 0x14, 0x06));
+    palette.setBrush(QPalette::Accent, QColor(0xb8, 0x88, 0x2e));
+    palette.setBrush(QPalette::Highlight, QColor(0x8F, 0x24, 0x24));
+    palette.setBrush(QPalette::HighlightedText, QColor(0xED, 0xED, 0xED));
+    palette.setBrush(QPalette::AlternateBase, QColor(0xc7, 0xc3, 0x6d));
     palette.setBrush(QPalette::Disabled, QPalette::Text, mix(text, window, 0.6));
     palette.setBrush(QPalette::Disabled, QPalette::Button, mix(button, window, 0.45));
     return palette;
