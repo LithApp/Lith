feb64d6d1e112889ca4e78fb5b5b859d001d72a8
Martin Bříza <m@rtinbriza.cz>
2024-10-11 07:31:44 +0200

    Update GitHub actions to more recent versions

--- dist/macos/build.sh
+++ dist/macos/build.sh
@@ -2,6 +2,6 @@
 
 mkdir build_macos
 cd build_macos
-export CMAKE_PREFIX_PATH=$Qt6_DIR/lib/cmake
+export CMAKE_PREFIX_PATH=$QT_ROOT_DIR/lib/cmake
 cmake .. -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" -DCMAKE_BUILD_TYPE=Release -DIMGUR_API_KEY=$IMGUR_API_KEY -DVERSION=$VERSION_NAME -DCMAKE_INSTALL_PREFIX=installed
 cmake --build . --parallel
