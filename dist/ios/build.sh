e9869c5bb7189cea769a2ae03bbdcb44b43abdf4
Martin Bříza <m@rtinbriza.cz>
2025-09-25 20:15:19 +0200

    Update build.sh
    Test build

--- dist/ios/build.sh
+++ dist/ios/build.sh
@@ -9,7 +9,8 @@ cd build_ios
 export QT_HOST_PATH="${QT_ROOT_DIR}/../macos"
 cmake .. -GXcode -DCMAKE_PREFIX_PATH=$QT_ROOT_DIR -DCMAKE_OSX_SYSROOT=iphoneos -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_TOOLCHAIN_FILE=$QT_ROOT_DIR/lib/cmake/Qt6/qt.toolchain.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DVERSION=$VERSION_NAME -DIMGUR_API_KEY=$IMGUR_API_KEY -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_STYLE="Manual"
 
-xcodebuild -project Lith.xcodeproj -scheme Lith -configuration RelWithDebInfo archive -archivePath Lith.xcarchive DEVELOPMENT_TEAM=Z52EFCPL6D
-xcodebuild -exportArchive -archivePath Lith.xcarchive -exportOptionsPlist ../dist/ios/exportOptions.plist -exportPath .
+xcodebuild -project Lith.xcodeproj -scheme Lith -configuration RelWithDebInfo archive -archivePath Lith.xcarchive DEVELOPMENT_TEAM=Z52EFCPL6 
+run: xcodebuild -scheme Lith -archivePath $PWD/Lith.xcarchive -destination 'platform=macOS,arch=x86_64'
+xcodebuild -exportArchive -archivePath $PWD/Lith.xcarchive -exportOptionsPlist ../dist/ios/exportOptions.plist -exportPath .
 
 cd ..
