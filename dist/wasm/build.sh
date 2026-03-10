adec3fbe1a0631627f63a672d8f77a62dfa18adf
Martin Bříza <m@rtinbriza.cz>
2026-03-10 17:34:32 +0100

    Update Qt to 6.10.2

--- dist/wasm/build.sh
+++ dist/wasm/build.sh
@@ -3,11 +3,11 @@
 set -e
 set -x
 
-mkdir build_wasm
+mkdir -p build_wasm
 pushd build_wasm
 
 export QT_HOST_PATH="${QT_ROOT_DIR}/../gcc_64"
 export CMAKE_PREFIX_PATH=$QT_ROOT_DIR/lib/cmake
 cmake .. -DCMAKE_BUILD_TYPE=Release -DIMGUR_API_KEY=$IMGUR_API_KEY -DCMAKE_INSTALL_PREFIX=installed -DCMAKE_TOOLCHAIN_FILE=$QT_ROOT_DIR/lib/cmake/Qt6/qt.toolchain.cmake
-cmake --build .
+cmake --build . --parallel
 cmake --install .
