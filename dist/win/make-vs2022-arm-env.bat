c396e1d56b1fa8ca0aa9ae40d7299bc62d44b53e
Martin Bříza <m@rtinbriza.cz>
2026-03-11 12:37:50 +0100

    Add win-arm64 and linux-arm64 builders, use macOS 26 in CI

---
+++ dist/win/make-vs2022-arm-env.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+set OLDPATH=%PATH%
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" arm64 > NUL:
+
+echo export INCLUDE='%INCLUDE%'
+echo export LIB='%LIB%'
+echo export LIBPATH='%LIBPATH%'
+
+call set NEWPATH=%%PATH:%OLDPATH%=%%
+set NEWPATH=%NEWPATH:C:=/c%
+set NEWPATH=%NEWPATH:\=/%
+set NEWPATH=%NEWPATH:;=:%
+echo export PATH="%NEWPATH%:$PATH"
